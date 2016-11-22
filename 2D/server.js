fb09f127361995ff25b30fee901c24dc5fea4d2f
Michael <mixa.yurin@yandex.ru>
2016-11-22 23:52:03 +0300

    2D: 32 tests pass

--- 2D/server.js
+++ 2D/server.js
@@ -2,26 +2,24 @@ var url = require('url');
 var express = require('express');
 var cors = require('cors');
 const rgbHex = require('rgb-hex');
-var onecolor = require('onecolor');
+// var onecolor = require('onecolor');
 // var colorString = require('color-string');
 
 var app = express();
 app.use(cors());
 app.get('/', function (req, res) {
+    console.log(req.query);
+    var out = 'Invalid color';
     if (req.query.color != undefined) {
-        console.log(`"${req.query.color}"`);
         var inputColor = req.query.color;
-        var out;
-        if (/[\#]+/g.test(inputColor) != true) { // /(rgb\([0-9\,\s]+\)){1}/.test(inputColor)
-            try {
-                // out = '#' + rgbHex(inputColor);
-                out = onecolor(inputColor).hex();
-                // var out = colorString.to.hex(colorString.get(inputColor));
-            } catch (error) {
-                console.log(error);
+        try {
+            if (/[\#]+/g.test(inputColor) != true) { // /(rgb\([0-9\,\s]+\)){1}/.test(inputColor)
+                out = '#' + rgbHex(inputColor);
+            } else {
                 inputColor = inputColor.replace('#', '').toLowerCase().trim();
                 if (/[^0-9a-fA-F]+/.test(inputColor)) {
-                    out = 'Invalid color2';
+                    out = 'Invalid color';
+                    console.log('Invalid color 1');
                 } else {
                     switch (inputColor.length) {
                         case 3:
@@ -31,18 +29,36 @@ app.get('/', function (req, res) {
                             out = '#' + inputColor;
                             break;
                         default:
-                            out = 'Invalid color3';
+                            out = 'Invalid color';
+                            console.log('Invalid color 2');
                             break;
                     }
                 }
             }
-        } else {
-            out = 'Invalid color4';
+        } catch (error) {
+            inputColor = inputColor.replace('#', '').toLowerCase().trim();
+            if (/[^0-9a-fA-F]+/.test(inputColor)) {
+                out = 'Invalid color';
+                console.log('Invalid color 1');
+            } else {
+                switch (inputColor.length) {
+                    case 3:
+                        out = '#' + inputColor[0] + inputColor[0] + inputColor[1] + inputColor[1] + inputColor[2] + inputColor[2]
+                        break;
+                    case 6:
+                        out = '#' + inputColor;
+                        break;
+                    default:
+                        out = 'Invalid color';
+                        console.log('Invalid color 2');
+                        break;
+                }
+            }
         }
-        res.send(out);
     }
+    res.send(out);
 });
 
 app.listen(3000, function () {
-    console.log('App listening on port 3000!');
+    console.log('App listening on http://localhost:3000/');
 });
