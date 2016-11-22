67c3eaca5d3a880c7908b647e86d943a917fb76b
Michael <mixa.yurin@yandex.ru>
2016-11-23 01:13:55 +0300

    2D: 35 tests pass

--- 2D/server.js
+++ 2D/server.js
@@ -1,9 +1,8 @@
 var url = require('url');
 var express = require('express');
 var cors = require('cors');
-const rgbHex = require('rgb-hex');
-// var onecolor = require('onecolor');
-// var colorString = require('color-string');
+var tinycolor = require("tinycolor2");
+
 
 var app = express();
 app.use(cors());
@@ -12,48 +11,30 @@ app.get('/', function (req, res) {
     var out = 'Invalid color';
     if (req.query.color != undefined) {
         var inputColor = req.query.color;
+        inputColor = inputColor.toLowerCase().trim();
         try {
-            if (/[\#]+/g.test(inputColor) != true) { // /(rgb\([0-9\,\s]+\)){1}/.test(inputColor)
-                out = '#' + rgbHex(inputColor);
+            if (/(rgb\([0-9\,\s]+\)){1}/.test(inputColor)) {
+                console.log('rgb');
+                var tcolor = tinycolor(inputColor.replace(/[\s\(\,)]+/g, ' ').trim());
+                console.log(`\t>> '${tcolor._originalInput}'`);
+                if (tcolor._originalInput == `rgb ${tcolor._r} ${tcolor._g} ${tcolor._b}`) {
+                    out = '#' + tcolor.toHex();
+                }
+            } else if (/(hsl\(.*\)){1}/.test(inputColor)) {
+                console.log('hsl');
+                out = '#' + tinycolor(inputColor.replace(/\%20/g, '').replace('#', '')).toHex();
             } else {
-                inputColor = inputColor.replace('#', '').toLowerCase().trim();
-                if (/[^0-9a-fA-F]+/.test(inputColor)) {
+                if (/[^0-9a-fA-F\#]+/g.test(inputColor)) {
                     out = 'Invalid color';
                     console.log('Invalid color 1');
                 } else {
-                    switch (inputColor.length) {
-                        case 3:
-                            out = '#' + inputColor[0] + inputColor[0] + inputColor[1] + inputColor[1] + inputColor[2] + inputColor[2]
-                            break;
-                        case 6:
-                            out = '#' + inputColor;
-                            break;
-                        default:
-                            out = 'Invalid color';
-                            console.log('Invalid color 2');
-                            break;
+                    inputColor = inputColor.replace('#', '')
+                    if (inputColor.length == 6 || inputColor.length == 3) {
+                        out = '#' + tinycolor(inputColor).toHex();
                     }
                 }
             }
         } catch (error) {
-            inputColor = inputColor.replace('#', '').toLowerCase().trim();
-            if (/[^0-9a-fA-F]+/.test(inputColor)) {
-                out = 'Invalid color';
-                console.log('Invalid color 1');
-            } else {
-                switch (inputColor.length) {
-                    case 3:
-                        out = '#' + inputColor[0] + inputColor[0] + inputColor[1] + inputColor[1] + inputColor[2] + inputColor[2]
-                        break;
-                    case 6:
-                        out = '#' + inputColor;
-                        break;
-                    default:
-                        out = 'Invalid color';
-                        console.log('Invalid color 2');
-                        break;
-                }
-            }
         }
     }
     res.send(out);
